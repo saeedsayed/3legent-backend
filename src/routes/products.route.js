b85bd45ad738f9acec61b7ddf455411916f04176
eng-saeed <eng.saeedsayed@gmail.com>
2025-09-22 08:54:55 +0300

    Add @vercel/speed-insights dependency and implement role-based access control for product routes

--- src/routes/products.route.js
+++ src/routes/products.route.js
@@ -1,16 +1,17 @@
 import express from "express";
 import { getAllProducts, getSingleProduct, createProduct, updateProduct, deleteProduct } from "../controllers/products.controller.js";
 import { CreateProductSchema } from "../middlewares/products.middleware.js";
-import { checkToken } from "../middlewares/auth.middleware.js";
+import { checkToken, restrictTo } from "../middlewares/auth.middleware.js";
+import roles from "../utils/roles.js";
 
 const router = express.Router();
 
 router.route('/')
     .get(getAllProducts)
-    .post(checkToken,CreateProductSchema(),createProduct);
+    .post(checkToken, restrictTo(roles.ADMIN), CreateProductSchema(), createProduct);
 router.route('/:id')
     .get(getSingleProduct)
-    .put(checkToken, updateProduct)
-    .delete(checkToken, deleteProduct);
+    .put(checkToken, restrictTo(roles.ADMIN), updateProduct)
+    .delete(checkToken, restrictTo(roles.ADMIN), deleteProduct);
 
 export default router;
