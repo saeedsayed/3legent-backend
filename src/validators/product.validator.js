1617b6a63f1820ad0b296f0923e97a4ecd348bd9
eng-saeed <eng.saeedsayed@gmail.com>
2025-09-29 00:35:11 +0300

    Fix product schema validation by renaming 'name' to 'title' and correcting price type to number

--- src/validators/product.validator.js
+++ src/validators/product.validator.js
@@ -1,8 +1,7 @@
 import {z as Z} from "zod";
 
 export const createProductSchema = Z.object({
-    name: Z.string("name is required").min(2, "name must be at least 2 characters").max(100),
+    title: Z.string("title is required").min(2, "title must be at least 2 characters").max(100),
     description: Z.string("description is required").min(10, "description must be at least 10 characters").max(1000),
-    // price: Z.string("price is required and must be a positive number").regex(/^\d+(\.\d+)?$/, "price must be a valid number").transform((val) => parseFloat(val)).min(0.1, "price must be at least 0.1"),
-    price: Z.string("price is required and must be a positive number").min(0.1, "price must be at least 0.1"),
+    price: Z.number("price is required and must be a positive number").min(0.1, "price must be at least 0.1"),
 });
