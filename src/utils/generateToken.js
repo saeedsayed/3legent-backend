4569474fccc8a042c37e36887bae4dfcdff8438e
saeed <120129486+saeedsayed@users.noreply.github.com>
2025-10-05 20:21:26 +0300

    Update generateToken.js

--- src/utils/generateToken.js
+++ src/utils/generateToken.js
@@ -6,7 +6,7 @@ export default (payload, res) => {
     // const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '10s' })
     res.cookie('jwt', token, {
         maxAge: expireIn * 24 * 60 * 60 * 1000, // convert form day to MS
-        httpOnly: true,
+        // httpOnly: true,
         sameSite: process.env.NODE_ENV === 'production' ? 'None' : 'Lax', // for cross-site cookie
         secure: process.env.NODE_ENV === 'production' // send cookie only on https
     })
