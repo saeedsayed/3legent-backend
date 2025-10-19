46a42fb6293ab1f2ec2d3bf757cfc164244f5e2e
saeed <120129486+saeedsayed@users.noreply.github.com>
2025-10-20 00:02:36 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -20,7 +20,9 @@ const app = express();
 const port = process.env.PORT || 4000;
 const DEVELOP_MODE = process.env.NODE_ENV === 'development';
 
-app.use(cors())
+app.use(cors({
+  origin:"http://localhost:3000"
+}))
 app.use(express.json());
 app.use(CookieParser())
 app.use(fileUpload({
