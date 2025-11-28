d2193a8289d05779126b7e30171924c2daf6afbe
eng-saeed <eng.saeedsayed@gmail.com>
2025-11-28 11:26:04 +0200

    Refactor database connection logic to prevent multiple connections and improve connection handling

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,9 +1,14 @@
 import mongoose from "mongoose";
-
+let isConnected = false;
 export const connectDB = async () => {
+  if (isConnected) {
+    console.log("already connected to mongoDB 👍");
+    return;
+  }
   try {
     console.log("connecting to mongoDB ... 🤌");
     await mongoose.connect(process.env.MONGO_DB_URI);
+    isConnected = true;
     console.log("mongoDB connected 👍");
   } catch (err) {
     console.log("mongoDB connection err", err);
